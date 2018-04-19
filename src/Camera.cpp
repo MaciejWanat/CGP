08741c0faafb99a627971767cc2eecc937a8567f
Axeris123 <axeris123@gmail.com>
2018-04-19 20:47:12 +0200

    Update

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -1,5 +1,9 @@
 #include "Camera.h"
 
+float Camera::yaw = 0;
+float Camera::pitch = 0;
+float Camera::roll = 0;
+
 glm::mat4 Core::createPerspectiveMatrix(float zNear, float zFar)
 {
 	const float frustumScale = 1.1f;
@@ -14,19 +18,42 @@ glm::mat4 Core::createPerspectiveMatrix(float zNear, float zFar)
 	return perspective;
 }
 
-glm::mat4 Core::createViewMatrix( glm::vec3 position, glm::vec3 forward, glm::vec3 up )
+//glm::mat4 Core::createViewMatrix( glm::vec3 position, glm::vec3 forward, glm::vec3 up )
+//{
+//	glm::vec3 side = glm::cross(forward, up);
+//
+//	// Trzeba pamietac o minusie przy ustawianiu osi Z kamery.
+//	// Wynika to z tego, ze standardowa macierz perspektywiczna zaklada, ze "z przodu" jest ujemna (a nie dodatnia) czesc osi Z.
+//	glm::mat4 cameraRotation;
+//	cameraRotation[0][0] = side.x; cameraRotation[1][0] = side.y; cameraRotation[2][0] = side.z;
+//	cameraRotation[0][1] = up.x; cameraRotation[1][1] = up.y; cameraRotation[2][1] = up.z;
+//	cameraRotation[0][2] = -forward.x; cameraRotation[1][2] = -forward.y; cameraRotation[2][2] = -forward.z;
+//
+//	glm::mat4 cameraTranslation;
+//	cameraTranslation[3] = glm::vec4(-position, 1.0f);
+//
+//	return cameraRotation * cameraTranslation;
+//}
+
+glm::mat4 Core::createViewMatrix(glm::vec3 position, float yaw, float pitch, float roll)
 {
-	glm::vec3 side = glm::cross(forward, up);
+	glm::mat4 Rx, Ry, Rz, M, T;
+	glm::mat4 Id = glm::mat4();
 
-	// Trzeba pamietac o minusie przy ustawianiu osi Z kamery.
-	// Wynika to z tego, ze standardowa macierz perspektywiczna zaklada, ze "z przodu" jest ujemna (a nie dodatnia) czesc osi Z.
-	glm::mat4 cameraRotation;
-	cameraRotation[0][0] = side.x; cameraRotation[1][0] = side.y; cameraRotation[2][0] = side.z;
-	cameraRotation[0][1] = up.x; cameraRotation[1][1] = up.y; cameraRotation[2][1] = up.z;
-	cameraRotation[0][2] = -forward.x; cameraRotation[1][2] = -forward.y; cameraRotation[2][2] = -forward.z;
+	glm::quat qPitch = glm::angleAxis(pitch, glm::vec3(1, 0, 0));
+	glm::quat qYaw = glm::angleAxis(yaw, glm::vec3(0, 1, 0));
+	glm::quat qRoll = glm::angleAxis(roll, glm::vec3(0, 0, 1));
 
-	glm::mat4 cameraTranslation;
-	cameraTranslation[3] = glm::vec4(-position, 1.0f);
+	glm::quat rotQuat = qPitch * qYaw * qRoll;
 
-	return cameraRotation * cameraTranslation;
+	glm::mat4 RotMatrix = glm::mat4_cast(rotQuat);
+
+	//Rx = glm::rotate(Id, roll, glm::vec3(0.0f, 0.0f, 1.0f));
+	//Ry = glm::rotate(Id, pitch, glm::vec3(0.0f, 1.0f, 0.0f));
+	//Rz = glm::rotate(Id, yaw, glm::vec3(1.0f, 0.0f, 0.0f));
+
+	T = glm::translate(Id, -position - 5.0f);
+	M = RotMatrix * T;
+
+	return M;
 }
--- src/main_5_1.cpp
+++ src/main_5_1.cpp
@@ -18,11 +18,19 @@ Core::Shader_Loader shaderLoader;
 obj::Model shipModel;
 obj::Model sphereModel;
 
+//Camera
+float cameraAngleX = 4.7;
+float cameraAngleY = 5;
 float cameraAngle = 0;
 glm::vec3 cameraPos = glm::vec3(-5, 0, 0);
 glm::vec3 cameraDir;
-
 glm::mat4 cameraMatrix, perspectiveMatrix;
+float yaw = 0.0;
+float pitch = 0.0;
+float roll = 0.0;
+
+float oldX = 0;
+float oldY = 0;;
 
 glm::vec3 lightDir = glm::normalize(glm::vec3(1.0f, -0.9f, -1.0f));
 
@@ -52,10 +60,42 @@ glm::mat4 createTranslationMatrixXYZ(float X, float Y, float Z) {
 	return translationMatrix;
 }
 
+void mouseMove(int x, int y)
+{
+	float sen = 0.05;
+	float margin = 5;
+
+	if (x - margin > oldX)
+	{
+		yaw += sen;
+		cameraAngleX += sen;
+	}
+	else if (x + margin < oldX)
+	{
+		yaw -= sen;
+		cameraAngleX -= sen;
+	}
+
+	if (y - margin > oldY)
+	{
+		pitch += sen;
+		cameraAngleY += sen;
+	}
+	else if (y + margin < oldY)
+	{
+		pitch -= sen;
+		cameraAngleY -= sen;
+	}
+
+	oldX = x;
+	oldY = y;
+}
+
 void keyboard(unsigned char key, int x, int y)
 {
 	float angleSpeed = 0.1f;
 	float moveSpeed = 0.1f;
+	float moveSpeedUpDown = 0.3f;
 	switch(key)
 	{
 	case 'z': cameraAngle -= angleSpeed; break;
@@ -64,16 +104,16 @@ void keyboard(unsigned char key, int x, int y)
 	case 's': cameraPos -= cameraDir * moveSpeed; break;
 	case 'd': cameraPos += glm::cross(cameraDir, glm::vec3(0,1,0)) * moveSpeed; break;
 	case 'a': cameraPos -= glm::cross(cameraDir, glm::vec3(0,1,0)) * moveSpeed; break;
+	case 'c': cameraPos += glm::cross(cameraDir, glm::vec3(1, 0, 0)) * moveSpeedUpDown; break;
+	case 'v': cameraPos -= glm::cross(cameraDir, glm::vec3(1, 0, 0)) * moveSpeedUpDown; break;
 	}
 }
 
 glm::mat4 createCameraMatrix()
 {
-	// Obliczanie kierunku patrzenia kamery (w plaszczyznie x-z) przy uzyciu zmiennej cameraAngle kontrolowanej przez klawisze.
-	cameraDir = glm::vec3(cosf(cameraAngle), 0.0f, sinf(cameraAngle));
-	glm::vec3 up = glm::vec3(0,1,0);
+	cameraDir = glm::vec3(cosf(cameraAngleX), 0.0f, sinf(cameraAngleX));
 
-	return Core::createViewMatrix(cameraPos, cameraDir, up);
+	return Core::createViewMatrix(cameraPos, yaw, pitch, roll);
 }
 
 void drawObjectColor(obj::Model * model, glm::mat4 modelMatrix, glm::vec3 color)
@@ -162,7 +202,7 @@ void parallel_transport() {
 		else {
 			B = (B / glm::length(B));
 			float theta = acosf(glm::dot(tangent[i],tangent[i+1]));
-			normal[i + 1] = glm::rotate(tangent[i], theta, B);
+			//normal[i + 1] = glm::rotate(theta, B) * tangent[i];
 		}
 		binormal[i] = glm::cross(tangent[i], normal[i]);
 		binormal[i] = glm::normalize(binormal[i]);
@@ -198,14 +238,14 @@ void renderScene()
 	//glm::mat4 shipModelMatrix = glm::translate(cameraPos + cameraDir * 0.5f + glm::vec3(0,-0.25f,0)) * glm::rotate(-cameraAngle + glm::radians(90.0f), glm::vec3(0,1,0)) * glm::scale(glm::vec3(0.25f));
 	ship_pos = glm::vec3(circle_points[pointCounter % 220].x, circle_points[pointCounter % 220].y, circle_points[pointCounter % 220].z);
 	glm::mat4 shipModelMatrix = createTranslationMatrixXYZ(ship_pos.x, ship_pos.y, ship_pos.z);
-	drawObjectColor(&shipModel, shipModelMatrix, glm::vec3(0.6f));
+	drawObjectColor(&shipModel, shipModelMatrix, glm::vec3(0.7f,0.0f,0.0f));
 	pointCounter++;
 
 	//przemieszczanie stateczków
 	for (int i = 0; i < spaceships.size(); i++)
 	{
 		glm::mat4 shipModelMatrix = glm::translate(spaceships[i].pos) * glm::scale(glm::vec3(0.10f));
-		drawObjectColor(&shipModel, shipModelMatrix, glm::vec3(0.6f));
+		drawObjectColor(&shipModel, shipModelMatrix, glm::vec3(0.0f,0.0f,0.7f));
 
 		glm::vec3 v1, v2, v3;
 		int m1 = 1;
@@ -215,10 +255,12 @@ void renderScene()
 		v1 = (ship_pos - spaceships[i].pos)/100;
 		v1 = m1 * v1;
 		//v2 = rule2: keep a distance away from other objects
+		if (glm::length(ship_pos - spaceships[i].pos) < 2)
+			spaceships[i].pos -= (ship_pos - spaceships[i].pos);
 		for (int j = 0; j < spaceships.size(); j++){
 			v2 = glm::vec3(0, 0, 0);
 			if (j != i) //boid which is near by
-				if (glm::length(spaceships[j].pos - spaceships[i].pos) < 0.08)
+				if (glm::length(spaceships[j].pos - spaceships[i].pos) < 0.5)
 					v2 -= (spaceships[j].pos - spaceships[i].pos);
 		}
 		v2 = m2 * v2;
@@ -233,14 +275,14 @@ void renderScene()
 
 		spaceships[i].vel += v1 + v2 + v3;
 		//limiting the speed
-		int vlim = 5;
+		int vlim = 1;
 		if (glm::length(spaceships[i].vel) > vlim)
 			spaceships[i].vel = (spaceships[i].vel / glm::length(spaceships[i].vel)) * vlim;
 
 		spaceships[i].pos += spaceships[i].vel;
 	}
 
-	drawObjectTexture(&sphereModel, glm::translate(glm::vec3(2,0,2)), glm::vec3(0.8f, 0.2f, 0.3f));
+	//drawObjectTexture(&sphereModel, glm::translate(glm::vec3(2,0,2)), glm::vec3(0.8f, 0.2f, 0.3f));
 	//drawObjectTexture(&sphereModel, glm::translate(glm::vec3(-2,0,-2)), glm::vec3(0.1f, 0.4f, 0.7f));
 
 	glutSwapBuffers();
@@ -253,7 +295,7 @@ void init()
 	programTexture = shaderLoader.CreateProgram("shaders/shader_tex.vert", "shaders/shader_tex.frag");
 	sphereModel = obj::loadModelFromFile("models/sphere.obj");
 	shipModel = obj::loadModelFromFile("models/spaceship.obj");
-	drawCircle(0, 0, 0, 10, 219);
+	drawCircle(0, 0, 0, 5, 219);
 	parallel_transport();
 	initialise_particles(50);
 }
@@ -274,11 +316,12 @@ int main(int argc, char ** argv)
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
 	glutInitWindowPosition(200, 200);
-	glutInitWindowSize(600, 600);
+	glutInitWindowSize(1024, 1024);
 	glutCreateWindow("OpenGL Pierwszy Program");
 	glewInit();
 
 	init();
+	glutMotionFunc(mouseMove);
 	glutKeyboardFunc(keyboard);
 	glutDisplayFunc(renderScene);
 	glutIdleFunc(idle);
